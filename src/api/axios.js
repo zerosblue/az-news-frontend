f8b57ee8c611ca09ddcccec3bb63cd4d4817ceb4
jungmyungkim <zerosblue7717@gmail.com>
2026-02-02 15:31:23 +0900

    deploy: update new ip 3.106.202.217

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   //baseURL: 'http://localhost:8080', // 백엔드 주소
- baseURL: 'http://52.63.95.38:8080', // ★ 네 아마존 서버 IP로 변경! 
+ baseURL: 'http://3.106.202.217:8080', // ★ 네 아마존 서버 IP로 변경! 
   withCredentials: true, // ★ 중요: 쿠키(세션)를 주고받으려면 이게 필수야!
 });
 
